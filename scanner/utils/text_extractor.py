43a184342836bf233759a38adeafd0e456b691eb
Daxpanchal17 <daxp170@gmail.com>
2026-04-07 19:37:32 +0530

    next

--- scanner/utils/text_extractor.py
+++ scanner/utils/text_extractor.py
@@ -2,8 +2,10 @@ import pdfplumber
 import docx
 import os
 import nltk
+from pdf2image import convert_from_path
+import pytesseract
 
-
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 def extract_text_from_pdf(file_path):
 
     text = ""
@@ -15,6 +17,21 @@ def extract_text_from_pdf(file_path):
     return text
 
 
+def extract_text_with_ocr(file_path):
+
+    POPPLER_PATH = r"C:\Users\daxp1\Downloads\Release-25.12.0-0\poppler-25.12.0\Library\bin"
+
+    images = convert_from_path(
+        file_path,
+        poppler_path=POPPLER_PATH
+    )
+
+    text = ""
+
+    for img in images:
+        text += pytesseract.image_to_string(img)
+
+    return text
 def extract_text_from_docx(file_path):
 
     document = docx.Document(file_path)
@@ -43,19 +60,26 @@ def split_into_sentences(text):
     return sentences
 
 
-
 def extract_text(file_path):
 
     extension = os.path.splitext(file_path)[1].lower()
 
+    text = ""
+
     if extension == ".pdf":
-        return extract_text_from_pdf(file_path)
+        text = extract_text_from_pdf(file_path)
+
+        # 🔥 fallback to OCR if no text found
+        if not text or len(text.strip()) == 0:
+            text = extract_text_with_ocr(file_path)
 
     elif extension == ".docx":
-        return extract_text_from_docx(file_path)
+        text = extract_text_from_docx(file_path)
 
     elif extension == ".txt":
-        return extract_text_from_txt(file_path)
+        text = extract_text_from_txt(file_path)
 
     else:
         return "Unsupported file format"
+
+    return text
